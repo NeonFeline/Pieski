fb188c88f1fa2b6320d0f9eff7558ff226870fdd
matraass11 <fedor.kurmanov@gmail.com>
2025-11-12 17:08:08 +0100

    dijkstra with random graph changes

---
+++ readInput.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <utility>
+
+using adjList_t = std::vector<std::vector<std::pair<size_t, size_t>>>; // (weight, neighbour)
+
+struct task_t {
+    adjList_t node_connections;
+    size_t source_node, target_node;
+};
+
+task_t read_input() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    size_t nodes_n, edges_n;
+    std::cin >> nodes_n >> edges_n;
+    
+    size_t source_node, target_node;
+    std::cin >> source_node >> target_node;
+    
+    adjList_t node_connections(
+        nodes_n, std::vector<std::pair<size_t, size_t>> {} // (weight, neighbour)
+    );
+    
+    for (size_t i = 0; i < edges_n; i++) {
+        size_t u, v, w1, w2;
+        std::cin >> u >> v >> w1 >> w2;
+        node_connections[u].emplace_back(w1, v);
+        node_connections[v].emplace_back(w2, u);
+    }
+    return {node_connections, source_node, target_node};
+}
+
+
+void printAdjList(const adjList_t& node_connections) {
+    for (size_t i = 0; i < node_connections.size(); i++) {
+        std::cout << i << ": ";
+        for (auto [weight, other_node]: node_connections[i]) {
+            std::cout << other_node << " for " << weight << ", ";
+        }
+        std::cout << "\n";
+    }
+}
